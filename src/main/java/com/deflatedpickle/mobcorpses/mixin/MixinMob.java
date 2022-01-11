00ac6205504cb62343e9622a682646831cf9b17d
DeflatedPickle <deflatedpickle@gmail.com>
2022-01-11 14:36:04 +0000

    Forgot the shearing sound

--- src/main/java/com/deflatedpickle/mobcorpses/mixin/MixinMob.java
+++ src/main/java/com/deflatedpickle/mobcorpses/mixin/MixinMob.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ShearsItem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.event.GameEvent;
@@ -33,9 +35,19 @@ public abstract class MixinMob {
         player
             .getStackInHand(interactionHand)
             .damage(1, player, (p) -> p.sendToolBreakStatus(interactionHand));
-      } else {
-        RedstoneOreBlock.spawnParticles(me.world, me.getBlockPos());
       }
+
+      me.world.playSound(
+          me.getX(),
+          me.getY(),
+          me.getZ(),
+          SoundEvents.ENTITY_SHEEP_SHEAR,
+          SoundCategory.PLAYERS,
+          1.0f,
+          1.0f,
+          false);
+
+      RedstoneOreBlock.spawnParticles(me.world, me.getBlockPos());
 
       cir.setReturnValue(ActionResult.SUCCESS);
     }
